909ff66ac2ad5e12fa38e1deda4697341435ab81
David Madison <dmadison@users.noreply.github.com>
2020-02-06 19:25:08 -0500

    Print Serial version on 32U4 boards
    Previously this was only checking for Teensy USB modes. As the 32U4 boards under Arduino always have CDC serial, this is a catch-all.
    
    In the future it would be better to make my own define for AVR boards with the built-in CDC definition, but as only 32U4 and Teensy boards are supported this will do fine for now.

--- Arduino/DJSpinRhythm/DJSpinRhythm.ino
+++ Arduino/DJSpinRhythm/DJSpinRhythm.ino
@@ -65,7 +65,7 @@ KeyboardButton navigateRight('d');
 EffectHandler fx(dj, EffectsTimeout);
 
 void setup() {
-	#ifdef USB_SERIAL_HID
+	#if defined(USB_SERIAL_HID) || defined(__AVR_ATmega32U4__)
 	Serial.begin(115200);
 	Serial.println("DJ Hero - Spin Rhythm XD Controller v0.0.0");
 	Serial.println("By David Madison, (c) 2020");
